7125770d003b3db25bbffcbc6a62986de3019105
gjacqual <gjacqual@student.21-school.ru>
2021-12-19 10:44:14 +0300

    first commit

---
+++ application.py
@@ -0,0 +1,8 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
